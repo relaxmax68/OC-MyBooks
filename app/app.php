6269b0c2f236f0d37bcb1e83c126f6f57a79ddd6
relaxmax <maxime.code@bbox.fr>
2015-04-04 11:45:10 +0200

    intégration des fichiers du cours

---
+++ app/app.php
@@ -0,0 +1,23 @@
+<?php
+
+use Symfony\Component\Debug\ErrorHandler;
+use Symfony\Component\Debug\ExceptionHandler;
+
+// Register global error and exception handlers
+ErrorHandler::register();
+ExceptionHandler::register();
+
+// Register service providers.
+$app->register(new Silex\Provider\DoctrineServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views',));
+
+// Register services.
+$app['dao.article'] = $app->share(function ($app) {
+    return new MicroCMS\DAO\ArticleDAO($app['db']);
+});
+
+$app['dao.comment'] = $app->share(function ($app) {
+    $commentDAO = new MicroCMS\DAO\CommentDAO($app['db']);
+    $commentDAO->setArticleDAO($app['dao.article']);
+    return $commentDAO;
+});
